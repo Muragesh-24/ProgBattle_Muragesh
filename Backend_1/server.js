aa2a2d89d2e94f9a50301839f35dad762463dddc
Muragesh-24 <nyamagoudmurgesh@gmail.com>
2025-05-18 22:27:46 +0530

    added

--- Backend_1/server.js
+++ Backend_1/server.js
@@ -30,7 +30,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(cookieParser());
-app.use(cors());  
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://prog-battale-frontend.vercel.app'],
+  credentials: true
+}));
 
 
 app.use(express.json());
